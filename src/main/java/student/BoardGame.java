4fab31d01ed80a81b9e84e48b6f9c86761a6de28
sun <1980987589@qq.com>
2025-03-08 21:52:43 -0800

    2

--- src/main/java/student/BoardGame.java
+++ src/main/java/student/BoardGame.java
@@ -8,41 +8,17 @@ import java.util.Objects;
  */
 public class BoardGame implements Comparable<BoardGame> {
 
-    /** Game name. */
     private final String name;
-    /** Unique game identifier. */
     private final int id;
-    /** Minimum players required. */
     private final int minPlayers;
-    /** Maximum players allowed. */
     private final int maxPlayers;
-    /** Maximum play time in minutes. */
     private final int maxPlayTime;
-    /** Minimum play time in minutes. */
     private final int minPlayTime;
-    /** Average difficulty rating. */
     private final double difficulty;
-    /** Game rank. */
     private final int rank;
-    /** Average user rating. */
     private final double averageRating;
-    /** Year the game was published. */
     private final int yearPublished;
 
-    /**
-     * Creates a new BoardGame object.
-     *
-     * @param name Game name
-     * @param id Unique identifier
-     * @param minPlayers Minimum players required
-     * @param maxPlayers Maximum players allowed
-     * @param minPlayTime Minimum play time in minutes
-     * @param maxPlayTime Maximum play time in minutes
-     * @param difficulty Average difficulty rating
-     * @param rank Game rank
-     * @param averageRating Average user rating
-     * @param yearPublished Year the game was published
-     */
     public BoardGame(String name, int id, int minPlayers, int maxPlayers, int minPlayTime,
                      int maxPlayTime, double difficulty, int rank,
                      double averageRating, int yearPublished) {
@@ -58,63 +34,46 @@ public class BoardGame implements Comparable<BoardGame> {
         this.yearPublished = yearPublished;
     }
 
-    /** @return the name of the board game. */
     public String getName() {
         return name;
     }
 
-    /** @return the game ID. */
     public int getId() {
         return id;
     }
 
-    /** @return the minimum number of players. */
     public int getMinPlayers() {
         return minPlayers;
     }
 
-    /** @return the maximum number of players. */
     public int getMaxPlayers() {
         return maxPlayers;
     }
 
-    /** @return the maximum play time. */
     public int getMaxPlayTime() {
         return maxPlayTime;
     }
 
-    /** @return the minimum play time. */
     public int getMinPlayTime() {
         return minPlayTime;
     }
 
-    /** @return the difficulty rating. */
     public double getDifficulty() {
         return difficulty;
     }
 
-    /** @return the rank of the game. */
     public int getRank() {
         return rank;
     }
 
-    /** @return the average rating. */
     public double getRating() {
         return averageRating;
     }
 
-    /** @return the year of publication. */
     public int getYearPublished() {
         return yearPublished;
     }
 
-    /**
-     * Returns the numeric value of the game attribute based on the given GameData type.
-     *
-     * @param col The GameData attribute
-     * @return The corresponding numeric value
-     * @throws IllegalArgumentException If the attribute is not numeric
-     */
     public double getNumericValue(GameData col) throws IllegalArgumentException {
         return switch (col) {
             case RATING -> averageRating;
@@ -130,13 +89,6 @@ public class BoardGame implements Comparable<BoardGame> {
         };
     }
 
-    /**
-     * Returns the string value of the game attribute based on the given GameData type.
-     *
-     * @param col The GameData attribute
-     * @return The corresponding string value
-     * @throws IllegalArgumentException If the attribute is not a string
-     */
     public String getStringValue(GameData col) throws IllegalArgumentException {
         if (col == GameData.NAME) {
             return name;
@@ -144,12 +96,6 @@ public class BoardGame implements Comparable<BoardGame> {
         throw new IllegalArgumentException("Invalid string column: " + col);
     }
 
-    /**
-     * Returns a formatted string containing the game name and the specified attribute value.
-     *
-     * @param col The GameData attribute
-     * @return A formatted string with the game name and the specified attribute
-     */
     public String toStringWithInfo(GameData col) {
         return switch (col) {
             case NAME -> name;
@@ -165,61 +111,42 @@ public class BoardGame implements Comparable<BoardGame> {
         };
     }
 
-    /**
-     * Returns a string representation of the BoardGame object.
-     *
-     * @return A string containing all game attributes in the correct order
-     */
     @Override
     public String toString() {
-        return String.format(
-                "BoardGame{name='%s', id=%d, minPlayers=%d, maxPlayers=%d, maxPlayTime=%d, minPlayTime=%d, " +
-                        "difficulty=%.1f, rank=%d, averageRating=%.1f, yearPublished=%d}",
-                name, id, minPlayers, maxPlayers, maxPlayTime, minPlayTime,
-                difficulty, rank, averageRating, yearPublished
-        );
+        return "BoardGame{name='" + name + "', id=" + id + ", minPlayers=" + minPlayers
+                + ", maxPlayers=" + maxPlayers + ", maxPlayTime=" + maxPlayTime
+                + ", minPlayTime=" + minPlayTime + ", difficulty=" + difficulty
+                + ", rank=" + rank + ", averageRating=" + averageRating
+                + ", yearPublished=" + yearPublished + "}";
     }
 
-
-    /**
-     * Checks if two BoardGame objects are equal.
-     *
-     * @param obj The object to compare
-     * @return True if the objects are equal, otherwise false
-     */
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
         BoardGame boardGame = (BoardGame) obj;
-        return id == boardGame.id &&
-                minPlayers == boardGame.minPlayers &&
-                maxPlayers == boardGame.maxPlayers &&
-                maxPlayTime == boardGame.maxPlayTime &&
-                minPlayTime == boardGame.minPlayTime &&
-                Double.compare(boardGame.difficulty, difficulty) == 0 &&
-                rank == boardGame.rank &&
-                Double.compare(boardGame.averageRating, averageRating) == 0 &&
-                yearPublished == boardGame.yearPublished &&
-                name.equals(boardGame.name);
+        return id == boardGame.id
+                && minPlayers == boardGame.minPlayers
+                && maxPlayers == boardGame.maxPlayers
+                && maxPlayTime == boardGame.maxPlayTime
+                && minPlayTime == boardGame.minPlayTime
+                && Double.compare(boardGame.difficulty, difficulty) == 0
+                && rank == boardGame.rank
+                && Double.compare(boardGame.averageRating, averageRating) == 0
+                && yearPublished == boardGame.yearPublished
+                && name.equals(boardGame.name);
     }
 
-    /**
-     * Computes the hash code for the BoardGame object.
-     *
-     * @return The hash code of the object
-     */
     @Override
     public int hashCode() {
-        return Objects.hash(name, id, minPlayers, maxPlayers, minPlayTime, maxPlayTime, difficulty, rank, averageRating, yearPublished);
+        return Objects.hash(name, id, minPlayers, maxPlayers, minPlayTime,
+                maxPlayTime, difficulty, rank, averageRating, yearPublished);
     }
 
-    /**
-     * Compares two BoardGame objects by name (case-insensitive).
-     *
-     * @param other The other BoardGame object
-     * @return Comparison result based on name
-     */
     @Override
     public int compareTo(BoardGame other) {
         return this.name.compareToIgnoreCase(other.name);
